66976de2df18632199a6cddab37c0cef8c164da9
hosseintalebi <htalebi@hotmail.com>
2018-02-04 17:46:49 -0700

    finally fixed the client server issue

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.get('/api/', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../', '/client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../', './client/build/index.html'));
 })
 
 const port = process.env.PORT || 5000
