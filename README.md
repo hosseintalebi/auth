a14418639a1bd3713bdbf5434f236cc771ab63d8
hosseintalebi <htalebi@hotmail.com>
2018-02-04 17:09:08 -0700

    Adds intial server

---
+++ README.md
@@ -0,0 +1,16 @@
+## Authentication
+
+You can access the demo here:
+[DEMO](https://hosseintalebi.github.io/auth/)
+
+### Technologies used:
+
+#### Front end:
+- React
+- Redux
+- Redux Sagas
+
+#### Backend:
+- Node.js
+- MongoDB
+- Express.js
---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+
+app.use(express.static(path.join(__dirname, '../', './client/build')))
+
+app.get('/api/', (req, res) => {
+  res.send({a: {b: 12}});
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../', '/client/build/index.html'));
+})
+
+const port = process.env.PORT || 5000
+app.listen(port);
+
+console.log(`App is running on port ${port}`);
