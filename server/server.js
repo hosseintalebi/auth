6d6c5d7bc8118da2d99a37a4053b2e5650474fe1
hosseintalebi <htalebi@hotmail.com>
2018-02-04 21:29:02 -0700

    Add user login route

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser')
 
 const { mongoose } = require('./db/mongoose')
 const { User } = require('./models/user')
-const { authenticate } = require('./middleware/authenticate') 
+const { authenticate } = require('./middleware/authenticate')
 
 const app = express()
 
@@ -27,10 +27,21 @@ app.post('/users', (req, res) => {
   })
 })
 
+app.post('/users/login', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password'])
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user)
+    })
+  }).catch((e) => {
+    res.status(400).send()
+  })
+})
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user)
 })
+
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../', './client/build/index.html'))
